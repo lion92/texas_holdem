531020d541961d3db25dcf7d3a1bc81f8eb69386
kriss <kriss.clotilde@gmail.com>
2023-11-29 14:09:13 +0100

    nettoyage du code

--- src/test/java/TirageDeCarteTest.java
+++ src/test/java/TirageDeCarteTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
